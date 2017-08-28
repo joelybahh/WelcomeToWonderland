6c5884c6982a203d0fb44f5de8ec38df05f93608
MatthewJ97 <matthewjones1256@gmail.com>
2017-08-28 14:34:50 +1000

    #46 added some changes to puzzle functionality

--- WelcomeToWonderland/Assets/Scripts/RoomOne/WW_LightPuzzle.cs
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/WW_LightPuzzle.cs
@@ -3,17 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WW_LightPuzzle : WW_Puzzle {
-    [SerializeField]
-    WW_StageLight[] m_lights;
-    [SerializeField]
+    [SerializeField] WW_StageLight[] m_lights;
+    [SerializeField] float m_timer;
 
-    float m_timer;
+    [SerializeField]
+    [Tooltip("The Number the Lights have to be assigned from 0-3. EXAMPLE Light1 = 2, light2 = 4, light3 = 1, light4 = 3. the key would be 2,4,1,3")] int[] m_PuzzleKey = { 2,3,1,4 };
+    bool PuzzleCorrect;
 
     private void Awake( ) {
         DisableAllLights();
-    }
-
-    private void Start() {
     }
 
     void DisableAllLights( ) {
@@ -24,23 +22,18 @@ public class WW_LightPuzzle : WW_Puzzle {
 
     private void Update( ) {
         m_timer += Time.deltaTime;
-        if ( m_timer > 0.0f && m_timer < 1.0f ) {
-            DisableAllLights();
-            m_lights[2].ToggleLight(true);
+        for ( int i = 0; i < m_lights.Length; i++ ) {
+            if ( !m_lights[i].GetPowered ) return;
         }
 
-        if ( m_timer > 1.0f && m_timer < 2.0f ) {
-            DisableAllLights();
-            m_lights[0].ToggleLight(true);
+        for ( int i = 0; i < m_lights.Length; i++ ) {
+            if(m_lights[i].m_SetId != m_PuzzleKey[i] ) {
+                PuzzleCorrect = false;
+            }
+            else { PuzzleCorrect = true;}
         }
 
-        if ( m_timer > 2.0f && m_timer < 3.0f ) {
-            DisableAllLights();
-            m_lights[1].ToggleLight(true); }
-
-        if ( m_timer > 3.0f && m_timer < 4.0f ) {
-            DisableAllLights();
-            m_lights[3].ToggleLight(true); }
+        
 
         if ( m_timer > 4.0f && m_timer < 6.0f ) { DisableAllLights(); }
 
--- WelcomeToWonderland/Assets/Scripts/RoomOne/WW_StageLight.cs
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/WW_StageLight.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class WW_StageLight : MonoBehaviour {
     [SerializeField] Light m_light;
-    [SerializeField]
-    bool m_isPowered;
-    private void Awake( ) {
-        m_isPowered = false;
-    }
+    [SerializeField] bool m_isPowered;
+    [SerializeField] static int m_identifer;
+    [SerializeField] public int m_SetId;
+ 
     public bool GetPowered{
         get {
             return m_isPowered;
         }
         set {
             m_isPowered = value;
+            if ( m_isPowered ) m_SetId = m_identifer++; 
             if (!m_isPowered) { m_light.enabled = false;}
             
         }
+    }
+    private void Awake( ) {
+        m_isPowered = false;
     }
 
     public void ToggleLight(bool a_bool ) {
