055a423df357d69d04e71f52cde14a2a98a2a6f2
Joel Gabriel <joelybahh@gmail.com>
2017-08-30 09:58:25 +1000

    Changed alot of things in regards to the scripts name

---
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/PuzzleHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace WW.Puzzles {
+    public class PuzzleHandler : MonoBehaviour {
+        [SerializeField]
+        List<Puzzle> m_puzzleTriggers;
+
+        [SerializeField]
+        UnityEvent m_Outcome;
+        private void Start() {
+            foreach ( Puzzle puzzle in m_puzzleTriggers ) {
+                puzzle.m_handler = this;
+            }
+        }
+        public void CheckCompleted() {
+            foreach ( Puzzle puzzle in m_puzzleTriggers ) {
+                if ( puzzle.m_completed == false ) {
+                    return;
+                } else {
+                    continue;
+                }
+            }
+            CompletePuzzle();
+
+        }
+        void CheckCompleted( int a_Override ) {
+            CompletePuzzle();
+        }
+
+        void CompletePuzzle() {
+
+        }
+    }
+}
---
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/StageLight.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WW.Puzzles {
+    public class StageLight : MonoBehaviour {
+        [SerializeField]
+        Light m_light;
+        [SerializeField]
+        bool m_isPowered;
+        [SerializeField]
+        public int m_SetId;
+
+        public bool GetPowered {
+            get {
+                return m_isPowered;
+            }
+            set {
+                m_isPowered = value;
+                if ( m_isPowered ) m_SetId = LightPuzzle.m_identifier++;
+                if ( !m_isPowered ) { m_SetId = 0; LightPuzzle.m_identifier--; m_light.enabled = false; }
+                Debug.Log(LightPuzzle.m_identifier);
+            }
+        }
+        private void Awake() {
+            m_isPowered = false;
+        }
+
+        public void ToggleLight( bool a_bool ) {
+            if ( m_isPowered ) m_light.enabled = a_bool;
+            else { m_light.enabled = false; }
+        }
+        public void Incorrect() {
+            m_light.color = Color.red;
+        }
+        public void Correct() {
+            m_light.color = Color.green;
+        }
+    }
+}
