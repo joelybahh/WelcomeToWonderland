4a27732978857898ca38b20bdceae265f8791732
Joel Gabriel <joelybahh@gmail.com>
2017-08-31 10:25:13 +1000

    I changed the scene somewhere somehow

---
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/PuzzleTracker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleTracker : MonoBehaviour {
+    
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
