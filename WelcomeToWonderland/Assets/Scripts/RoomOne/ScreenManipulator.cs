035aca0fbaadc7fabddc56799304beecff18f20a
MatthewJ97 <MatthewJ97@users.noreply.github.com>
2017-09-11 11:03:00 +1000

    #bomb timer added and a way of manipulating "screens"

---
+++ WelcomeToWonderland/Assets/Scripts/RoomOne/ScreenManipulator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class ScreenManipulator : MonoBehaviour {
+
+   public UnityEvent m_on;
+   public UnityEvent m_off;
+
+    
+}
