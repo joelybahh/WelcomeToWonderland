d9b6c53919f80bf3a2bfc3cb01bae358cba3f17c
AIE\s171304 <roboonpsn@gmail.com>
2017-10-05 12:02:51 +1100

    sidewall_fbx screenshothelper new proj, vertcol shader

---
+++ WelcomeToWonderland/Assets/Scripts/Helpers/ScreenShotHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShotHelper : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         if(Input.GetMouseButtonUp(2)) {
+            ScreenCapture.CaptureScreenshot("Screenshot.png", 10);
+        }
+    }
+}
